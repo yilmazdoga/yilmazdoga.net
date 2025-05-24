adcfb721db5301a185b1d5394a973be589cab6ff
yilmazdoga <yilmaz.doga@gmail.com>
2025-05-24 20:37:37 +0100

    removed theme stuff

--- js/scripts.js
+++ js/scripts.js
@@ -27,32 +27,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  const themeSwitch = document.getElementById('theme-switch');
-  const root = document.documentElement;
-  const images = document.querySelectorAll('img[data-dark][data-light]'); // Select images with data attributes
-
-  // Function to update image sources based on the theme
-  const updateImages = (isLightTheme) => {
-    images.forEach((img) => {
-      img.src = isLightTheme ? img.dataset.light : img.dataset.dark;
-    });
-  };
-
-  // Check for saved theme in localStorage
-  const isLightTheme = localStorage.getItem('theme') === 'light';
-  if (isLightTheme) {
-    root.classList.add('light-theme');
-    themeSwitch.checked = true;
-  }
-  updateImages(isLightTheme);
-
-  // Add event listener for theme switch
-  themeSwitch.addEventListener('change', () => {
-    const isLightTheme = themeSwitch.checked;
-    root.classList.toggle('light-theme', isLightTheme);
-    localStorage.setItem('theme', isLightTheme ? 'light' : 'dark');
-    updateImages(isLightTheme);
-  });
-});
